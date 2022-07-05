9aad508e477f1a603945c3d504005c2fbdaa1e21
Joshua <joshua.jacobowitz@yahoo.de>
2022-07-05 21:07:06 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,99 @@
+type Methods<T, E> = {
+  expect: (message: string) => T;
+  expectError: (message: string) => E;
+  unwrap: () => T;
+  unwrapOr: (_default: T) => T;
+  unwrapOrElse: (fn: (error: E) => T) => T;
+  map: <U>(fn: (value: T) => U) => Result<U, E>;
+  mapError: <F>(fn: (error: E) => F) => Result<T, F>;
+  and: <U>(result: Result<U, E>) => Result<U, E>;
+  andThen: <U>(fn: (value: T) => Result<U, E>) => Result<U, E>;
+  or: <F>(result: Result<T, F>) => Result<T, F>;
+  orElse: <F>(fn: (error: E) => Result<T, F>) => Result<T, F>;
+};
+
+type Attributes<T, E> = { ok: true; value: T } | { ok: false; error: E };
+
+export type Result<T, E> = Attributes<T, E> & Methods<T, E>;
+
+function createResult<T, E>(init: Attributes<T, E>): Result<T, E> {
+  return {
+    ...init,
+    expect(message) {
+      if (init.ok) return init.value;
+      throw new Error(message);
+    },
+    expectError(message) {
+      if (init.ok) throw new Error(message);
+      return init.error;
+    },
+    unwrap() {
+      if (init.ok) return init.value;
+      throw new Error();
+    },
+    unwrapOr(_default) {
+      if (init.ok) return init.value;
+      return _default;
+    },
+    unwrapOrElse(fn) {
+      if (init.ok) return init.value;
+      return fn(init.error);
+    },
+    and(result) {
+      if (init.ok) return result;
+      return createResult(init);
+    },
+    andThen(fn) {
+      if (init.ok) return fn(init.value);
+      return createResult(init);
+    },
+    or(result) {
+      if (init.ok) return createResult(init);
+      return result;
+    },
+    orElse(fn) {
+      if (init.ok) return createResult(init);
+      return fn(init.error);
+    },
+    map(fn) {
+      if (init.ok) return createResult({ ok: true, value: fn(init.value) });
+      return createResult(init);
+    },
+    mapError(fn) {
+      if (init.ok) return createResult(init);
+      return createResult({ ok: false, error: fn(init.error) });
+    },
+  };
+}
+
+export const Result = {
+  ok<T, E>(value: T): Result<T, E> {
+    return createResult({ ok: true, value });
+  },
+  error<T, E>(error: E): Result<T, E> {
+    return createResult({ ok: false, error });
+  },
+  equals<T, E>(a: Result<T, E>, b: Result<T, E>): boolean {
+    if (a.ok) return b.ok && a.value === b.value;
+    if (!b.ok) return a.error === b.error;
+    return false;
+  },
+  from<T, E>(fn: () => T): Result<T, E> {
+    try {
+      return Result.ok(fn());
+    } catch (e: any) {
+      return Result.error(e);
+    }
+  },
+  wrap<T, E, A extends Array<any> = []>(
+    fn: (...args: A) => T
+  ): (...args: A) => Result<T, E> {
+    return (...args) => {
+      try {
+        return Result.ok(fn(...args));
+      } catch (e: any) {
+        return Result.error(e);
+      }
+    };
+  },
+};
---
+++ test.ts
@@ -0,0 +1,186 @@
+import { Result } from "./index";
+import assert from "assert";
+
+const { ok, error, equals, from, wrap } = Result;
+
+// ok
+
+assert.deepEqual(JSON.parse(JSON.stringify(ok(1))), { ok: true, value: 1 });
+
+// error
+
+assert.deepEqual(JSON.parse(JSON.stringify(error(1))), { ok: false, error: 1 });
+
+// equals
+
+assert(equals(ok(1), ok(1)));
+assert(equals(error(1), error(1)));
+
+assert(!equals(ok(1), ok(2)));
+assert(!equals(error(1), error(2)));
+
+assert(!equals(error<number, number>(1), ok(1)));
+assert(!equals(ok<number, number>(1), error(1)));
+
+// from
+
+assert(
+  equals(
+    from(() => 1),
+    ok(1)
+  )
+);
+
+assert(
+  equals(
+    from<void, number>(() => {
+      throw 1;
+    }),
+    error(1)
+  )
+);
+
+// wrap
+
+assert(equals(wrap((x) => x)(1), ok(1)));
+
+assert(
+  equals(
+    wrap<number, number, [number]>((x) => {
+      throw x;
+    })(1),
+    error(1)
+  )
+);
+
+// expect
+
+try {
+  const x = error("emergency failure");
+  x.expect("Testing expect");
+} catch (e) {
+  assert(e instanceof Error);
+  assert(e.message === "Testing expect");
+}
+
+assert.equal(ok(10).expect("Testing expectError"), 10);
+
+// expectError
+
+try {
+  const x = ok(10);
+  x.expectError("Testing expectError");
+} catch (e) {
+  assert(e instanceof Error);
+  assert(e.message === "Testing expectError");
+}
+
+assert.equal(error(10).expectError("Testing expectError"), 10);
+
+// unwrap
+
+try {
+  const x = error("emergency failure");
+  x.unwrap();
+} catch (e) {
+  assert(e instanceof Error);
+  assert(e.message === "");
+}
+
+assert.equal(ok(10).unwrap(), 10);
+
+// unwrapOr
+
+assert.equal(ok(9).unwrapOr(2), 9);
+assert.equal(error("error").unwrapOr(2), 2);
+
+// unwrapOrElse
+
+const count = (x: string) => x.length;
+
+assert.equal(ok<number, string>(2).unwrapOrElse(count), 2);
+assert.equal(error("foo").unwrapOrElse(count), 3);
+
+// map
+
+assert(
+  equals(
+    ok("foo").map((s) => s.length),
+    ok(3)
+  )
+);
+
+assert(
+  equals(
+    error<string, string>("error").map((s) => s.length),
+    error("error")
+  )
+);
+
+// mapError
+
+assert(
+  equals(
+    error("error").mapError((s) => s.length),
+    error(5)
+  )
+);
+
+assert(
+  equals(
+    ok<string, string>("foo").mapError((s) => s.length),
+    ok("foo")
+  )
+);
+
+// and
+
+assert(
+  equals(ok<number, string>(2).and(error("late error")), error("late error"))
+);
+
+assert(equals(error("early error").and(ok("foo")), error("early error")));
+
+assert(equals(error("not a 2").and(error("late error")), error("not a 2")));
+
+assert(
+  equals(ok(2).and(ok("different result type")), ok("different result type"))
+);
+
+// andThen
+
+assert(
+  equals(
+    ok(2).andThen((i) => ok(i.toString())),
+    ok("2")
+  )
+);
+
+assert(
+  equals(
+    error<number, number>(2).andThen((i) => ok(i.toString())),
+    error(2)
+  )
+);
+
+// or
+
+assert(equals(ok(2).or(error("late error")), ok(2)));
+
+assert(equals(error<number, string>("early error").or(ok(2)), ok(2)));
+
+assert(equals(error("not a 2").or(error("late error")), error("late error")));
+
+assert(equals(ok(2).or(ok(100)), ok(2)));
+
+// orElse
+
+const sq = (x: number): Result<number, number> => ok(x * x);
+const err = (x: number): Result<number, number> => error(x);
+
+assert(equals(ok<number, number>(2).orElse(sq).orElse(sq), ok(2)));
+assert(equals(ok<number, number>(2).orElse(err).orElse(sq), ok(2)));
+
+assert(equals(error<number, number>(3).orElse(sq).orElse(err), ok(9)));
+
+assert(equals(error<number, number>(3).orElse(err).orElse(err), error(3)));
